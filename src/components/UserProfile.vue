b14231f9dce1fa22ba020f2b5ece2ed0d5daacec
felix <gathagefelix@gmail.com>
2023-07-07 11:16:40 +0300

    boldify some items

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -3,7 +3,7 @@
         <div class="container mt-5">
             <div class="row">
                 <div class="col-md-4">
-                    <h1>Blogger Details</h1>
+                    <h1 style="font-weight: bolder;">Blogger Details</h1>
                     <div class="image">
                         <img src="../assets/images/profile_user.jpg" alt="User Photo" class="img-fluid rounded-circle" style="width: 200px;  margin-left: 55px;">
                         <h3 class="text-center mt-3">{{ user.first_name }}</h3>
@@ -13,7 +13,7 @@
                 </div>
 
                 <div class="col-md-8">
-                    <h1>Recent Posts</h1>
+                    <h1 style="font-weight: bolder;" >Recent Posts</h1>
                     <div class="row">
                         <div v-if="posts.length === 0" class="col-md-12">
                             <div class="card bg-light-red">
@@ -26,7 +26,7 @@
                             <div class="card mb-3">
                                 <img :src="'http://127.0.0.1:8000'+post.image" class="card-img-top" alt="Post Image" style="height: 350px;">
                                 <div class="card-body">
-                                    <h5 class="card-title">{{ post.title }}</h5>
+                                    <h5 style="font-weight: bold;" class="card-title">{{ post.title }}</h5>
                                     <div class="d-flex justify-content-between">
                                         <small class="text-muted">Likes: </small>
                                         <small class="text-muted">Comments: </small> 
